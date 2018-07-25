b3adb3fcf814996c0f636f8c83b35c24728cafbc
Tuyen Le Chau <Tuyen.LeChau@nashtechglobal.com>
2018-07-25 11:04:27 +0700

    rename application

--- cucumber/src/main/java/com/example/cucumber/MainApplication.java
+++ cucumber/src/main/java/com/example/cucumber/MainApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CucumberApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CucumberApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 }
