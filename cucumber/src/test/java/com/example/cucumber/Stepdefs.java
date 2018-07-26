376abed766b3b4f85f3bdf195e6a411cf77fe6f6
Tuyen Le Chau <Tuyen.LeChau@nashtechglobal.com>
2018-07-26 11:31:21 +0700

    add test with list string

--- cucumber/src/test/java/com/example/cucumber/Stepdefs.java
+++ cucumber/src/test/java/com/example/cucumber/Stepdefs.java
@@ -1,5 +1,9 @@
 package com.example.cucumber;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -20,5 +24,21 @@ public class Stepdefs {
 
 	@Then("^my belly should growl$")
 	public void my_belly_should_growl() {
+	}
+
+	@Given("^I have (\\d+) types of food$")
+	public void i_have_types_of_food(int numberOfTypes, List<String> foodTypes) {
+		assertEquals(numberOfTypes, foodTypes.size());
+		assertEquals("beef", foodTypes.get(2));
+	}
+
+	@When("^I choice (\\d+) type of food$")
+	public void i_choice_type_of_food(int choiceOfTypes) {
+		assertEquals(1, choiceOfTypes);
+	}
+
+	@Then("^I should get (.*?)$")
+	public void i_should_get_food(String typeOfFood) {
+		assertEquals("beef", typeOfFood);
 	}
 }
--- cucumber/src/test/resources/com/example/cucumber/belly.feature
+++ cucumber/src/test/resources/com/example/cucumber/belly.feature
@@ -4,3 +4,11 @@ Feature: Belly
 		Given I have 42 cukes in my belly
 		When I wait 1 hour
 		Then my belly should growl
+
+	Scenario: choice beef
+		Given I have 3 types of food
+			|bread|
+			|pork|
+			|beef|
+		When I choice 1 type of food
+		Then I should get beef
